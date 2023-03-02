aab085a6e6374f280439293aba513ca59d71caf2
Mihir Pathak <mihirpathak267@gmail.com>
2023-03-02 23:50:39 +0530

    Finish the Drum Kit.
    Add all drum kit buttons in index.html and then style it.
    Use a function that takes in the button pressed to animate the
    button by adding and removing the .pressed css style. Use a
    function to make a switch case for every drum sound to play.
    Use a for loop to add an event listener for clicks to all buttoons
    and trigger sounds and button animation. Finally add an event listener
    for key presses to do the same.

---
+++ index.js
@@ -0,0 +1,58 @@
+// function to only make sounds with param key
+function doitall(currentKey){
+    sounds(currentKey);
+    buttonAnimation(currentKey);
+}
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
+function sounds(key){
+    switch(key){
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        default:
+            break;
+    }
+}
+
+// for loop to add event listener to all buttons and trigger sounds function passing this.innerHTML as param
+for (var i = 0; i < document.querySelectorAll(".set button").length; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function(){
+        doitall(this.innerHTML);
+    });
+    
+}
+// event listener to trigger sounds function passing key pressed as param
+document.addEventListener("keydown", function(e){
+    doitall(e.key);
+});
